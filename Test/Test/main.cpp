698a74c2a097f450cd22349811809d840d63f986
latuanvinh1998 <latuanvinh1998@gmail.com>
2019-05-30 00:40:26 +0700

    summon sleep

--- Test/Test/main.cpp
+++ Test/Test/main.cpp
@@ -1,3 +1,9 @@
+/*
+ * Test.cpp
+ *
+ * Created: 5/30/2019 12:29:56 AM
+ * Author : Elite Phantom
+ */ 
 #include <avr/io.h>
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
@@ -8,23 +14,14 @@ int main(void)
 	DDRC = 0xFF;
 	DDRD = 0x00;
 	PORTD = 0x00;
-	DDRB = 0xFF;
-	GICR |= (1 << INT0);
 	MCUCR &= ~((1 << ISC01) | (1 << ISC00));
+	MCUCR |= (1<<ISC11);
+	GICR |=(1<<INT1)|(1<<INT0);
 	sei();
-	
 	while (1)
 	{
 		_delay_ms(500);
 		PORTC = ~PORTC;
-		_delay_ms(500);
-		set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-
-		sleep_enable();
-		sei();
-		sleep_cpu();
-		sleep_disable();
-		cli();
 	}
 }
 
@@ -34,3 +31,12 @@ ISR(INT0_vect)
 	PORTC = 0x00;
 	_delay_ms(1000);
 }
+ISR(INT1_vect)
+{
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	sleep_enable();
+	sleep_cpu();
+	sleep_disable();
+	cli();
+}
+
